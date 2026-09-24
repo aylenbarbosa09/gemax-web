c00eff85cd0aef193c1355fbdd35706eb10adc35
aylenbarbosa09 <aylenbarbosa09@gmail.com>
2026-09-24 15:19:17 -0300

    Secciones Hombre Mujer y Zapas

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def inicio():
     return render_template("index.html")
 
+@app.route("/hombre")
+def hombre():
+    return render_template("hombre.html")
+
+@app.route("/mujer")
+def mujer():
+    return render_template("mujer.html")
+
+@app.route("/zapas")
+def zapas():
+    return render_template("zapas.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
